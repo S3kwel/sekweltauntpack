f9a650d663aab200d662f9213a1523069853dea2
Dustin Hickman <43394966+S3kwel@users.noreply.github.com>
2020-04-10 18:06:32 -0400

    Final

---
+++ addon/lua/autorun/sh_phe_additional_taunts.lua
@@ -0,0 +1,107 @@
+-- In here you can add two team taunts without seperating them.
+local taunts = {}
+
+-- Begin Table: Hunters
+taunts.Hunter = {
+	["Guuuh!"]						=	"vo/k_lab/ba_guh.wav",
+	["If you See Dr. Breen"]		= 	"vo/streetwar/rubble/ba_tellbreen.wav",
+	["All We Gotta Do Is"] = "taunts/sekweltauntpack/hunters/All We Gotta Do Is.mp3",
+	["As your Vice President"] = "taunts/sekweltauntpack/hunters/As your Vice President.mp3",
+	["At Strickland"] = "taunts/sekweltauntpack/hunters/At Strickland.mp3",
+	["Boggle"] = "taunts/sekweltauntpack/hunters/Boggle.mp3",
+	["I'm Gonna Kick your Ass"] = "taunts/sekweltauntpack/hunters/Come Back Here I'm Gonna Kick your Ass.mp3",
+	["Compare your Lives to Mine"] = "taunts/sekweltauntpack/hunters/Compare your Lives to Mine.mp3",
+	["Dallas Mower Expo"] = "taunts/sekweltauntpack/hunters/Dallas Mower Expo.mp3",
+	["Dallas, Sallad"] = "taunts/sekweltauntpack/hunters/Dallas, Salad.mp3",
+	["Die"] = "taunts/sekweltauntpack/hunters/Die.mp3",
+	["Doing Alcohol"] = "taunts/sekweltauntpack/hunters/Doing Alcohol.mp3",
+	["Everyone's Dead"] = "taunts/sekweltauntpack/hunters/Everyone's Dead.mp3",
+	["Hooh Yeeaheeah"] = "taunts/sekweltauntpack/hunters/Hooh Yeeaheeah.mp3", 
+	["I am Going to Smack Everyone"] = "taunts/sekweltauntpack/hunters/I am Going to Smack Everyone.mp3",
+	["I killed Fiddy Men"] = "taunts/sekweltauntpack/hunters/I Killed Fiddy Men.mp3",
+	["I will Eat your Babies Bitch"] = "taunts/sekweltauntpack/hunters/I Will Eat your Babies Bitch.mp3",
+	["I'm the Lizard King"] = "taunts/sekweltauntpack/hunters/I'm the Lizard King.mp3",
+	["Identity Thef is Not a Joke"] = "taunts/sekweltauntpack/hunters/Identity Theft is Not a Joke.mp3",
+	["Just Try and Run Well"] = "taunts/sekweltauntpack/hunters/Just Try and Run Well.mp3",
+	["My Favorite Lamp"] = "taunts/sekweltauntpack/hunters/My Favorite Lamp.mp3", 
+	["Rumbling"] = "taunts/sekweltauntpack/hunters/Rumbling.mp3", 
+	["Someone's Gonna Kick his Ass"] = "taunts/sekweltauntpack/hunters/Someone's Gonna Kick his Ass.mp3", 
+	["The Good Thing About Death"] = "taunts/sekweltauntpack/hunters/The Good Thing About Death.mp3", 
+	["Uh oh, Nerds"] = "taunts/sekweltauntpack/hunters/Uh oh Nerds.mp3",
+	["Uh, Harley Davidson"] = "taunts/sekweltauntpack/hunters/Uh Harley Davidson.mp3",
+	["What"] = "taunts/sekweltauntpack/hunters/What.mp3", 
+	["You can Run but you can't Hide"] = "taunts/sekweltauntpack/hunters/You can Run but you Cant Hide.mp3", 
+	["You know what it is Bitch"] = "taunts/sekweltauntpack/hunters/You Know What it is Bitch.mp3", 
+	["You life prolonging Fools"] = "taunts/sekweltauntpack/hunters/You Life Prolonging Fools.mp3", 
+	["You Must Die"] = "taunts/sekweltauntpack/hunters/You Must Die.mp3", 
+	["You Will Wake Up In"] = "taunts/sekweltauntpack/hunters/You Will Wake Up In.mp3",
+	["Friday Escape"] = "taunts/sekweltauntpack/hunters/Friday Escape.mp3",
+	["Penis?"] = "taunts/sekweltauntpack/hunters/penis.mp3",
+	["Pocket Sand"] = "taunts/sekweltauntpack/hunters/pocket sand.mp3",
+	["Try Crack"] = "taunts/sekweltauntpack/hunters/try crack.mp3",
+	
+	-- Add more Hunters Taunt here...
+}
+-- Begin Table: Props
+taunts.Props = {
+	["Windows XP Shutdown"]			=	"taunts/ph_enhanced/ext_xp_off.wav",
+	["Windows XP Startup"]			=	"taunts/ph_enhanced/ext_xp_start.wav",
+	["Advanced Darkness" ] = "taunts/sekweltauntpack/props/Advanced Darkness.mp3",
+	["Bodies are for Hookers and Fat People" ] = "taunts/sekweltauntpack/props/Bodies are for Hookers and Fat People.mp3",
+	["Bombs?"] = "taunts/sekweltauntpack/props/Bombs.mp3",
+	["Buttlicker!"] = "taunts/sekweltauntpack/props/Buttlicker.mp3",
+	["Dang"] = "taunts/sekweltauntpack/props/Dang.mp3",
+	["Doing Alcohol"] = "taunts/sekweltauntpack/props/Doing Alcohol.mp3",
+	["Dwight, you Ignorant Slut!"] = "taunts/sekweltauntpack/props/Dwight you Ignorant Slut.mp3",
+	["Firmly Grasp It!"] = "taunts/sekweltauntpack/props/Firmly Grasp It.mp3",
+	["Ggeh!"] = "taunts/sekweltauntpack/props/Ggeh.mp3",
+	["Hank Scream 1"] = "taunts/sekweltauntpack/props/Hank Scream 1.mp3",
+	["I am the Dayman"] = "taunts/sekweltauntpack/props/I am the Dayman.mp3",
+	["Declaring Bankruptcy"] = "taunts/sekweltauntpack/props/I Declare Bankruptcy.mp3",
+	["I wonder What's for Dinner"] = "taunts/sekweltauntpack/props/I Wonder Whats for Dinner.mp3",
+	["I'm a Little Worried about Being a Slut"] = "taunts/sekweltauntpack/props/I'm a Little Worried about Being a Slut.mp3",
+	["It's a Giraffe"] = "taunts/sekweltauntpack/props/It's a Giraffe.mp3",
+	["Kill Me Already"] = "taunts/sekweltauntpack/props/Kill Me Already.mp3",
+	["Krusty Krab Pizza"] = "taunts/sekweltauntpack/props/Krusty Krab Pizza.mp3",
+	["Let's Go Already!"] = "taunts/sekweltauntpack/props/Lets Go Already.mp3",
+	["Let's go you Murderer!"] = "taunts/sekweltauntpack/props/Lets Go you Murderer.mp3",
+	["Murder me by Surprise"] = "taunts/sekweltauntpack/props/Murder Me By Surprise.mp3",
+	["My Antiques"] = "taunts/sekweltauntpack/props/My Antiques.mp3",
+	["PENcil"] = "taunts/sekweltauntpack/props/Pencil.mp3",
+	["Rock me Mom's Spaghetti"] = "taunts/sekweltauntpack/props/Rock Me Moms Spaghetti.mp3",
+	["Shshhshaah"] = "taunts/sekweltauntpack/props/ShShhShaaah.mp3",
+	["Sound Effects!"] = "taunts/sekweltauntpack/props/Sound Effects.mp3",
+	["This Map is too Confusing!"] = "taunts/sekweltauntpack/props/This Map is too Confusing.mp3",
+	["Turn off the Duke"] = "taunts/sekweltauntpack/props/Turn of the Dook.mp3",
+	["Wanna Kill all Humans?"] = "taunts/sekweltauntpack/props/Wanna Kill All Humans.mp3",
+	["Where are my Testicles?"] = "taunts/sekweltauntpack/props/Where are my Testicles.mp3",
+	["Why don't you let your Gun do the Talking?"] = "taunts/sekweltauntpack/props/Why Dont you let your Gun do the Talking.mp3",
+	["Wild Card"] = "taunts/sekweltauntpack/props/Wild Card.mp3",
+	["You're a Hurtful Slut, Bob!"] = "taunts/sekweltauntpack/props/Youre a Hurtful Slut Bob.mp3",
+	["Who's been drawing dicks?"] = "taunts/sekweltauntpack/props/whos been drawing dicks.mp3",
+	["Inspector Snoop"] = "taunts/sekweltauntpack/props/inspector snoop.mp3",
+	["Darth Cena"] = "taunts/sekweltauntpack/props/Darth Cena.mp3",
+	["It's too violent!"] = "taunts/sekweltauntpack/props/too violent.mp3"
+	-- Add more Props Taunt here...
+}
+
+-- if everything's done with above, let's add them as the list. 
+-- They will be automatically added as soon as the game loads!
+hook.Add("Initialize", "AddCustomTaunts", function()
+    timer.Simple(math.random(4,6), function()
+        for propTaunt,propPath in pairs(taunts.Props) do
+            PHE:AddCustomTaunt(TEAM_PROPS,propTaunt,propPath)
+            resource.AddSingleFile("sound/"..propPath)
+        end
+        for hunterTaunt,hunterPath in pairs(taunts.Hunter) do 
+            PHE:AddCustomTaunt(TEAM_HUNTERS,hunterTaunt,hunterPath)
+            resource.AddSingleFile("sound/"..hunterPath)
+        end
+
+        PHE:RefreshTauntList()
+    end)
+end)
+
+
+-- You can also add your custom taunts outside from this scope with your own [ list.Set("PHE.Custom<Prop/Hunter>Taunts", "Taunt Name", "Your Taunt Path") ]
+
